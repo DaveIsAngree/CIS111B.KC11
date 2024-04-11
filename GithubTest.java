a2b15623fd022244ed32e168749ca82219177559
DaveIsAngree <sangree.d@gmail.com>
2024-04-10 22:52:56 -0400

    Create GithubTest.java
    GithubTest.java is a simple program used as a stand-in to test git and github functionality.

---
+++ GithubTest.java
@@ -0,0 +1,13 @@
+public class GithubTest
+{
+
+   public static void main (String[] args)
+   {
+      System.out.println("This is a test of Git and Github!");
+      for (int i = 1; i <= 20; i++)
+      {
+         System.out.print(i + " ");
+      }
+   }
+
+}
